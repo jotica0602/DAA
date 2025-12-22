7b0091ca75888dac30e144d68706cc0274c253f7
ATKPowder <67365703+jotica0602@users.noreply.github.com>
2025-12-22 03:55:55 -0500

    modified

--- src/test.py
+++ src/test.py
@@ -1,26 +1,28 @@
 import networkx as nx
 from itertools import combinations
 from collections import defaultdict
+import heapq
 import random
 import time
 
-def find(x,parent):
+def set_of(x,parent):
     if parent[x] == x: return x
-    parent[x] = find(parent[x],parent)
+    parent[x] = set_of(parent[x],parent)
     return parent[x]
 
 def merge(x,y,parent):
-    px,py = find(x,parent),find(y,parent)
+    px,py = set_of(x,parent),set_of(y,parent)
     if px == py: return False
     parent[py] = px
     return True
 
+# region Generador
 def generate_instance(n,
                       edge_prob=0.4,
                       w_min=1,
                       w_max=10,
                       violation_prob=0.4,
-                      seed=None):
+                      seed=None) -> tuple[list[list[list[tuple]]],dict[int:int]]:
     '''
     :param n: cantidad de vértices del grafo
     :param edge_prob: probabilidad de que entre un par de vértices haya una arista
@@ -28,6 +30,8 @@ def generate_instance(n,
     :param w_max: costo máximo de arista
     :param violation_prob: probabilidad de que un vértice viole su restricción de grado
     :param seed: semilla
+
+    returns:
     '''
     if seed is not None:
         random.seed(seed)
@@ -85,13 +89,14 @@ def get_cost(T:nx.Graph):
     '''
     return sum(w['weight'] for _,_,w in T.edges.data())
 
-def ok(degree_bound,T:nx.Graph):
+def is_feasable(degree_bound,T:nx.Graph):
     '''
     Verifica que cada nodo del árbol cumpla con la restricción de grado.
     '''
     return all(T.degree(node) <= degree_bound[node] for node in T)
 
-def get_dcmst(G:nx.Graph,degree_bound) -> tuple[int,nx.Graph]: # O(2^m), m = |E|
+# region Fuerza Bruta
+def bruteforce(G:nx.Graph,degree_bound) -> tuple[int,nx.Graph]: # O(2^m), m = |E|
     '''
     :param G: instancia a resolver
     :param degree_bound: mapeo de las restricciones de grado para cada vértice.
@@ -99,6 +104,7 @@ def get_dcmst(G:nx.Graph,degree_bound) -> tuple[int,nx.Graph]: # O(2^m), m = |E|
     Construye un grafo a partir del conjunto de aristas y obtiene el DC-MST.
     '''
 
+    n = len(G.nodes)
     m = len(G.edges)
     min_cost = float('inf') # fijamos una cota superior para podas
 
@@ -110,72 +116,192 @@ def get_dcmst(G:nx.Graph,degree_bound) -> tuple[int,nx.Graph]: # O(2^m), m = |E|
             T.add_nodes_from(G)
             T.add_edges_from([(u,v,w) for u,v,w in comb])
             actual_cost = get_cost(T)                   # obtenemos el costo de aristas del grafo craedo con esta combinación
+            if len(T.edges) < n - 1: continue           # las combinaciones de aristas con menos de n-1 las descartamos
             if actual_cost >= min_cost: continue        # podamos aquellas soluciones que excedan nuestra mejor solución
-            if nx.is_tree(T) and ok(degree_bound,T):    # si el costo es potencialmente mejor que el mejor costo obtenido, verificamos que sea un árbol y que se cumpla la restricción de grado 
+            if nx.is_tree(T) and is_feasable(degree_bound,T):    # si el costo es potencialmente mejor que el mejor costo obtenido, verificamos que sea un árbol y que se cumpla la restricción de grado 
                 min_cost = get_cost(T)
     return min_cost,T
 
-def dual_method(G:nx.Graph, MST:nx.Graph, degree_bounds):
-    for i in list(MST.nodes):
-        if MST.degree(i) <= degree_bounds[i]: continue
-        while MST.degree(i) > degree_bounds[i]:
-            p = {}
-            erj_exists = False
-            for j in list(MST.neighbors(i)):
-                w_eij = MST[i][j]['weight']
-                MST.remove_edge(i,j)
-                erj,pj = get_replacement_edge(j,w_eij,MST,G,degree_bounds)
-                if erj is not None:
-                    erj_exists |= True
-                    p[j] = [erj,pj]
-                MST.add_weighted_edges_from([(i,j,w_eij)])
+
+
+# region Kernelización
+def reduction_dcmst(G:nx.Graph,degree_bounds):
+    '''
+    :param G: Grafo inicial
+    :type G: nx.Graph
+    :param degree_bounds: restricciones de grado para cada vértice
+
+    Devuelve un grafo T_star candidato a conectar para hallar un DCST
+    '''
+    G = G.copy()
+    if len(G) <= 2: return G,G
+    # Inicialización
+    T_star = nx.Graph()
+    T_star.add_nodes_from(G)
+    
+    changed = True
+    while changed:
+        changed = False
+        # Paso 1: Teorema 2
+        to_remove = set()
+        for u,v,_ in G.edges.data():
+            if degree_bounds[u] == degree_bounds[v] == 1:
+                to_remove.add((u,v))
+                changed = True
+        G.remove_edges_from(to_remove)
+        to_remove.clear()
+    
+        # Paso 2: Teorema 1
+        for u in G:
+            if G.degree(u) == 1:
+                v = list(G.neighbors(u))[0]
+                T_star.add_edge(u,v,weight=G[u][v]['weight'])
+                G.remove_edge(u,v)
+                to_remove.add(u)
+                changed = True
+        G.remove_nodes_from(to_remove)
+        to_remove.clear()
+
+        # Paso 3: Teorema 3
+        for vk in G:
+            if G.degree(vk) != 2: continue
+            vi,vj = G.neighbors(vk)
+            G_prime = G.copy()
+            G_prime.remove_node(vk)
+            if not nx.has_path(G_prime,vi,vj):
+                T_star.add_edge(vi,vk,weight=G[vi][vk]['weight'])
+                T_star.add_edge(vj,vk,weight=G[vj][vk]['weight'])
+                G.remove_edge(vi,vk)
+                G.remove_edge(vj,vk)
+                to_remove.add(vk)
+                changed = True
+        G.remove_nodes_from(to_remove)
+    return G,T_star
+
+def kruskal_dcst(G:nx.Graph,T_star:nx.Graph,degree_bounds):
+    uf = {v:v for v in T_star}
+    for u,v in T_star.edges:
+        merge(u,v,uf)
+
+    q = [(w['weight'],u,v) for u,v,w in G.edges.data()]
+    heapq.heapify(q)
+
+    while q and len(T_star.edges) < len(T_star) - 1:
+        w,u,v = heapq.heappop(q)
+        if set_of(u,uf) == set_of(v,uf): continue 
+        if T_star.degree(u) + 1 > degree_bounds[u]: continue 
+        if T_star.degree(v) + 1 > degree_bounds[v]: continue
+        merge(u,v,uf)
+        T_star.add_edge(u,v,weight=w)
+
+    return T_star
+
+# region Heurísticas    
+def dual_method(G:nx.Graph, T_star:nx.Graph, degree_bounds):
+        for i in T_star.nodes:
+            if T_star.degree(i) <= degree_bounds[i]: continue
+            while T_star.degree(i) > degree_bounds[i]:
+                p = {}
+                ers_exists = False
+
+                for j in T_star.neighbors(i):
+                    w_eij = T_star[i][j]['weight']
+                    ers,pj = get_replacement_edge(i,j,w_eij,T_star.copy(),G,degree_bounds)
+                    if ers is not None:
+                        ers_exists = True
+                        p[j] = (ers,pj)
+                
+                if not ers_exists: return T_star # No existe solución factible
+                
+                j,ers,pj = get_best_replacement_edge(p)
+                r,s = ers
+                w_ers = pj + w_eij
+                T_star.remove_edge(i,j)
+                T_star.add_weighted_edges_from([(r,s,w_ers)])
+
+        return T_star
             
-            if not erj_exists: # No existe una solución
-                return float('inf'),MST
-            erj,pj = get_best_replacement_edge(p)
-            r,j = erj
-            MST.remove_edge(i,j)
-            w_erj = pj + w_eij
-            print(f'PJ: {pj} WERJ:{w_erj}')
-            MST.add_weighted_edges_from([(r,j,w_erj)])
-    return get_cost(MST), MST
-            
-def get_replacement_edge(j, w_eij: int, MST: nx.Graph, G: nx.Graph, degree_bounds):
+def get_replacement_edge(i,j, w_eij: int, MST: nx.Graph, G: nx.Graph, degree_bounds):
     best = float('inf')
-    components = list(nx.connected_components(MST))
-    ccj = next(c for c in components if j in c)
-    erj = None
-    for r_ in G.neighbors(j):
-        if r_ in ccj:
-            continue
-        w_erj = G[r_][j]['weight']
-        pj = w_erj - w_eij
-        if pj < best and MST.degree(r_) + 1 <= degree_bounds[r_]:
-            best = pj
-            erj = (r_, j)
-    return erj, best
+    deg_j = MST.degree(j)
+    MST.remove_edge(i,j)
+    cc1,cc2 = nx.connected_components(MST)
+    ccj = cc1 if j in cc1 else cc2
+    ers = None
+    for s in ccj:
+        for r in G.neighbors(s):
+            if r in ccj: continue
+            w_ers = G[r][s]['weight']
+            pj = w_ers - w_eij
+            
+            r_is_valid = MST.degree(r) + 1 <= degree_bounds[r]
+            s_is_valid = (s == j and deg_j <= degree_bounds[j]) or MST.degree(s) + 1 <= degree_bounds[s]
+            
+            if pj < best and r_is_valid and s_is_valid:
+                best = pj
+                ers = (r,s)
+    return ers, best
 
 def get_best_replacement_edge(p:dict[tuple,int]):
-    erj,pj = sorted(p.values(),key=lambda x:x[1])[0]
-    return erj,pj 
+    if len(p) == 0:
+        print('No hay ningún j válido.')
+        return
+    best = float('inf')
+    j = None
+    for k in p:
+        _,pj = p[k]
+        if pj < best:
+            j = k
+    ers,pj = p[j]
+    return j,ers,pj
 
-for i in range(2,12):
-    edges,degree_bounds = generate_instance(i,seed=time.time())
-    # print(f'Test para {i} vértices y {len(edges)} aristas \n')
-    # Construimos G
-    G = nx.Graph()
-    G.add_weighted_edges_from(edges)
+# region Zona de Pruebas
+G = nx.Graph()
+G.add_weighted_edges_from([('a','b',5),('b','c',3),('c','e',1),('c','d',9),('c','f',4),('e','f',2),('f','d',17),('d','h',8),('h','g',3),('g','i',1),('g','j',5),('i','j',2)])
+degree_bounds = {'e':1,'f':1,'a':2,'b':2,'d':2,'g':2,'h':2,'i':2,'j':2,'c':4}
 
-    # Aplicamos dual method
-    T = nx.minimum_spanning_tree(G)
-    s = time.time()
-    min_cost,T = dual_method(G,T,degree_bounds)
-    f = time.time()
-    # print(f'Dual method: \n- costo: {min_cost}\n- tiempo: {f-s}')
+G,T_star = reduction_dcmst(G,degree_bounds)
+T_star = kruskal_dcst(G,T_star,degree_bounds)
+print(nx.is_tree(T_star))
+print(get_cost(T_star))
 
-    # Hallamos el costo por fuerza bruta
-    s = time.time()
-    min_cost,T = get_dcmst(G,degree_bounds)
-    f = time.time()
-    # print(f'Fuerza bruta:\n- costo: {min_cost} \n- tiempo: {f-s}\n')
-    
+
+for _ in range(20):
+    for i in range(2,100):
+        edges,degree_bounds = generate_instance(i,seed=time.time())
+        print(f'Test para {i} vértices y {len(edges)} aristas \n')
+        # Construimos G
+        G = nx.Graph()
+        G.add_weighted_edges_from(edges)
+
+        if len(G.edges) < len(G) - 1: 
+            print('Hay menos aristas de las requeridas para que el grafo sea conexo.')
+            continue 
+
+        # Kernelización
+        s = time.time()
+        G_prime,T_star = reduction_dcmst(G,degree_bounds)
+        T_star = kruskal_dcst(G_prime,T_star,degree_bounds)
+        f = time.time()
+        is_tree = nx.is_tree(T_star)
+        is_ok = False
+        if is_tree: is_ok = is_feasable(degree_bounds,T_star)
+        kernelization_cost = get_cost(T_star)
+        print(f'Kernelización: \n- costo: {kernelization_cost if is_tree and is_ok else float('inf')}\n- tiempo: {f-s}')
+
+        # Dual Method
+        T_star = nx.minimum_spanning_tree(G)
+        s = time.time()
+        T_star = dual_method(G,T_star,degree_bounds)
+        f = time.time()
+        is_tree = nx.is_tree(T_star)
+        is_ok = False
+        if is_tree: is_ok = is_feasable(degree_bounds,T_star)
+        dual_cost = get_cost(T_star)
+        print(f'Método Dual: \n- costo: {dual_cost if is_tree and is_ok else float('inf')}\n- tiempo: {f-s}')
+        
+        # Hallamos el costo por fuerza bruta
+        # s = time.time()
+        # bruteforce_cost,T2 = bruteforce(G,degree_bounds)
+        # f = time.time()
+        # print(f'Fuerza bruta:\n- costo: {bruteforce_cost} \n- tiempo: {f-s}\n')
