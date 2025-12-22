a146fdc834354e4b177b968a7874f9db37439353
ATKPowder <67365703+jotica0602@users.noreply.github.com>
2025-12-22 04:16:28 -0500

    agregado: docstring

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,12 @@
+'''
+Funciones de utilidad
+- set_of(x,parent): devuelve el representante de la clase de equivalencia de x.
+- merge(x,y,parent): une las clases de equivalencia de x e y si no son la misma.
+
+- get_cost(G): calcula el costo de las aristas del grafo que recibe como parámetro.
+- is_feasable(T,degree_bounds): devuelve True si todos los vértices del árbol respetan su restricción de grado.
+'''
+
 import networkx as nx
 
 def set_of(x,parent):
@@ -11,11 +20,11 @@ def merge(x,y,parent):
     parent[py] = px
     return True
 
-def get_cost(T:nx.Graph):
+def get_cost(G:nx.Graph):
     '''
-    Calcula el costo de un árbol sumando los pesos de todas sus aristas.
+    Calcula el costo de un grafo sumando los pesos de todas sus aristas.
     '''
-    return sum(w['weight'] for _,_,w in T.edges.data())
+    return sum(w['weight'] for _,_,w in G.edges.data())
 
 def is_feasable(degree_bound,T:nx.Graph):
     '''
